1afe05426483045f9703b8115b0db5d0df549cfa
trinhminhduc9x <duc1997th@gmail.com>
2022-09-13 14:57:42 +0700

    add

---
+++ ss2_thiet ke va tao CSDL/BAI 2/SalesManager.sql
@@ -0,0 +1,35 @@
+DROP DATABASE IF EXISTS SalesManager;
+create database SalesManager;
+use SalesManager;
+
+create table customer(
+cID int primary key auto_increment not null,
+cName varchar(45),
+cAge int
+);
+
+create table ordera(
+oID int primary key auto_increment not null,
+oDate  date,
+oTotal int,
+cID int,
+ FOREIGN KEY (cID)
+        REFERENCES customer (cID)
+);
+
+
+create table product(
+pID int primary key auto_increment not null,
+pName varchar(45),
+pPrice int
+);
+
+create table orderDetail(
+oID int ,
+pID int ,
+odQTY int,
+ FOREIGN KEY (oID)
+        REFERENCES ordera (oID),
+ FOREIGN KEY (pID)
+        REFERENCES product (pID)
+);
